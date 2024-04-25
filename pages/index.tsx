4d92ade9a8cb0e1edc0f2407c998df6b096395cb
Paarth Madan <paarth.mdn@gmail.com>
2024-04-25 11:40:46 -0400

    Make starter blank by default

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,8 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
-import Tutorial from "@/components/tutorial";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  return (
-    <div className="w-screen h-screen">
-      <div className="delete-div-after-tutorial">
-        <Tutorial />
-      </div>
-    </div>
-  );
+  return <div className="w-screen h-screen"></div>;
 }
