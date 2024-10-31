c051adfb413e5bc54238c7a0d4b61790d739f0c6
petergok <petergok@hotmail.com>
2024-10-31 10:07:13 -0400

    Added all images

--- next.config.js
+++ next.config.js
@@ -3,6 +3,12 @@ const nextConfig = {
   reactStrictMode: true,
   images: {
     unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+    ],
   },
 };
 
